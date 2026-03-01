8191b21bb8576088729aece2983f7dff4c58869c
AYUSH KOTHE <ayushkothe441@gmail.com>
2026-03-01 19:30:44 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
 import streamlit as st
 from src.pipeline.predict_pipeline import PredictionPipeline
-
 st.set_page_config(
     page_title="Support Ticket Classification System",
     page_icon="🎫",
-    layout="centered"
-)
-
+    layout="centered")
 st.title("🎫 Support Ticket Classification")
 st.write("Enter your support ticket description below to predict Ticket Type and Priority.")
-
-# =========================
-# Text Input
-# =========================
-
 user_input = st.text_area(
     "Enter Ticket Description",
     height=150,
-    placeholder="Example: My product keeps crashing after the latest update..."
-)
-
-# =========================
-# Predict Button
-# =========================
-
+    placeholder="Example: My product keeps crashing after the latest update...")
 if st.button("Predict"):
-
     if user_input.strip() == "":
         st.warning("Please enter a ticket description.")
     else:
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     train_data_path, test_data_path = obj.initiate_data_ingestion()
 
     transformation_obj = DataTransformation()
-    train_arr, test_arr = transformation_obj.data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr=transformation_obj.data_transformation(train_data_path,test_data_path)
 
     train = ModelTrainer()
-    report_type,report_priority,best_type_name,best_priority_name=train.initiate_model_trainer(train_arr, test_arr)
+    best_type_name,best_priority_name=train.initiate_model_trainer(train_arr, test_arr)
 
     print(best_type_name,best_priority_name)
